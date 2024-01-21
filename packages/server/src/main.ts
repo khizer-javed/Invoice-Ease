cae08cf0530fb362334d6710fccc80e97ac0786b
Khizer Javed Iqbal <khizerjaved25@gmail.com>
2024-01-22 01:50:49 +0500

    done

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -5,15 +5,6 @@ import { join } from 'path';
 import { PORT } from './constants';
 
 async function main() {
-  console.log('STRIPE_KEY', process.env.STRIPE_KEY);
-  console.log('DB_HOST', process.env.DB_HOST);
-  console.log('DB_USERNAME', process.env.DB_USERNAME);
-  console.log('DB_PASSWORD', process.env.DB_PASSWORD);
-  console.log('DB_DATABASE', process.env.DB_DATABASE);
-  console.log('DB_DIALECT', process.env.DB_DIALECT);
-  console.log('DB_PORT', process.env.DB_PORT);
-  console.log('DB_CLIENT', process.env.DB_CLIENT);
-
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.use(express.json({ limit: '50mb' }));
